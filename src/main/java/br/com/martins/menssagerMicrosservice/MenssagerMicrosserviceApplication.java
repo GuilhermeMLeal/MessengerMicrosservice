5563494f418c06681c9a961d890f2e2f49ae2828
Guilherme <guilherme.mleal.11@gmail.com>
2024-11-14 09:25:34 -0300

    feat(project): initialized project with base CRUD

---
+++ src/main/java/br/com/martins/menssagerMicrosservice/MenssagerMicrosserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.martins.menssagerMicrosservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenssagerMicrosserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenssagerMicrosserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/martins/menssagerMicrosservice/logging/LoggingAspect.java
@@ -0,0 +1,72 @@
+package br.com.martins.menssagerMicrosservice.logging;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    private static final Logger LOGGER = LogManager.getLogger(LoggingAspect.class);
+
+    @Autowired private HttpServletRequest request;
+    @Autowired private HttpServletResponse response;
+
+    @Before("within(@org.springframework.web.bind.annotation.RestController *)")
+    public void logRequest(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String methodName = signature.getDeclaringTypeName() + "." + signature.getName();
+        Object[] args = joinPoint.getArgs();
+
+        LOGGER.debug(
+                "Incoming Request: Method: {}, Path: {}, QueryString: {}, Method: {}, Arguments: {}",
+                request.getMethod(),
+                request.getRequestURI(),
+                request.getQueryString(),
+                methodName,
+                Arrays.toString(args));
+    }
+
+    @AfterReturning(
+            pointcut = "within(@org.springframework.web.bind.annotation.RestController *)",
+            returning = "result")
+    public void logResponse(JoinPoint joinPoint, Object result) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String methodName = signature.getDeclaringTypeName() + "." + signature.getName();
+
+        LOGGER.debug(
+                "Outgoing Response: Status: {}, Method: {}, Path: {}, Response: {}",
+                response.getStatus(),
+                methodName,
+                request.getRequestURI(),
+                result);
+    }
+
+    @AfterThrowing(
+            pointcut = "within(@org.springframework.web.bind.annotation.RestController *)",
+            throwing = "ex")
+    public void logException(JoinPoint joinPoint, Exception ex) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        String methodName = signature.getDeclaringTypeName() + "." + signature.getName();
+
+        LOGGER.error(
+                "Exception in Request: Status: {}, Method: {}, Path: {}, Exception: {}",
+                response.getStatus(),
+                methodName,
+                request.getRequestURI(),
+                ex.getMessage(),
+                ex);
+    }
+}
